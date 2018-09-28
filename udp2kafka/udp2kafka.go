a7ff5552a442d7adfb4d9a61985bfc5815c45881
Afsheen Bigdeli <afsheenb@namecast.net>
2018-09-28 18:08:37 +0000

    moved listen outside of go func

--- udp2kafka/udp2kafka.go
+++ udp2kafka/udp2kafka.go
@@ -44,13 +44,13 @@ func main() {
 	count := 0
 
 	wg.Add(2)
+	listen, err := net.Listen("tcp", ":514")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listen.Close()
 
 	go func() {
-		listen, err := net.Listen("tcp", ":514")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer listen.Close()
 
 		for {
 			conn, err := listen.Accept()
@@ -59,104 +59,103 @@ func main() {
 				log.Fatal(err)
 			}
 
-
-		scanner := bufio.NewScanner(conn)
-		for scanner.Scan() {
-			dataBuf := string(scanner.Text())
-			p := strings.Split(string(dataBuf), "@")
-			/* producer, err := sarama.NewAsyncProducer(broker, kafkaConfig)
-			if err != nil {
-				fmt.Println("Critical error connecting to kafka broker: %v", err)
-			}
-			*/
-			reqtopic := string(p[0])
-			resptopic := string(p[2])
-
-			if reqtopic == "httpreq" {
-				reqmsg := &sarama.ProducerMessage{
-					Topic: string(reqtopic),
-					Value: sarama.StringEncoder(p[1]),
-				}
-				reqproducer.Input() <- reqmsg
-
-				data := string(p[1])
-				pingdom_in_httpreq := 0
-				dataMap := make(map[string]interface{})
-				err := json.Unmarshal([]byte(data), &dataMap)
+			scanner := bufio.NewScanner(conn)
+			for scanner.Scan() {
+				dataBuf := string(scanner.Text())
+				p := strings.Split(string(dataBuf), "@")
+				/* producer, err := sarama.NewAsyncProducer(broker, kafkaConfig)
 				if err != nil {
-					fmt.Println(err)
+					fmt.Println("Critical error connecting to kafka broker: %v", err)
 				}
+				*/
+				reqtopic := string(p[0])
+				resptopic := string(p[2])
 
-				value, _ := gabs.ParseJSON([]byte(data))
-				ua := value.Path("device.ua").String()
-				ip := value.Path("device.ip").String()
-				host_to_httpreq_tracker := value.Path("page_name").String()
-				if host_to_httpreq_tracker == "\"pingdom.the-ozone-project.com\"" {
-					c, _ := statsd.New(statsd_host)
-					pingdom_in_httpreq++
-					c.Namespace = "logger."
-					c.Incr("pingdom_to_httpreq_snplow", nil, float64(pingdom_in_httpreq))
+				if reqtopic == "httpreq" {
+					reqmsg := &sarama.ProducerMessage{
+						Topic: string(reqtopic),
+						Value: sarama.StringEncoder(p[1]),
+					}
+					reqproducer.Input() <- reqmsg
+
+					data := string(p[1])
+					pingdom_in_httpreq := 0
+					dataMap := make(map[string]interface{})
+					err := json.Unmarshal([]byte(data), &dataMap)
+					if err != nil {
+						fmt.Println(err)
+					}
+
+					value, _ := gabs.ParseJSON([]byte(data))
+					ua := value.Path("device.ua").String()
+					ip := value.Path("device.ip").String()
+					host_to_httpreq_tracker := value.Path("page_name").String()
+					if host_to_httpreq_tracker == "\"pingdom.the-ozone-project.com\"" {
+						c, _ := statsd.New(statsd_host)
+						pingdom_in_httpreq++
+						c.Namespace = "logger."
+						c.Incr("pingdom_to_httpreq_snplow", nil, float64(pingdom_in_httpreq))
+					}
+					subject.SetUseragent(ua)
+					subject.SetIpAddress(ip)
+					sdj := sp.InitSelfDescribingJson("iglu:tech.hereford/httpreqs/jsonschema/2-0-4", dataMap)
+					tracker.TrackSelfDescribingEvent(sp.SelfDescribingEvent{Event: sdj})
+					fmt.Println(data)
 				}
-				subject.SetUseragent(ua)
-				subject.SetIpAddress(ip)
-				sdj := sp.InitSelfDescribingJson("iglu:tech.hereford/httpreqs/jsonschema/2-0-4", dataMap)
-				tracker.TrackSelfDescribingEvent(sp.SelfDescribingEvent{Event: sdj})
-				fmt.Println(data)
-			}
-			if resptopic == "bidresponse" {
-				data := string(p[3])
-				dataMap := make(map[string]interface{})
-				err := json.Unmarshal([]byte(data), &dataMap)
-				if err != nil {
-					fmt.Println(err)
-				}
-				respmsg := &sarama.ProducerMessage{
-					Topic: string(resptopic),
-					Value: sarama.StringEncoder(p[3]),
-				}
-				respproducer.Input() <- respmsg
-				value, _ := gabs.ParseJSON([]byte(data))
-				ua := value.Path("ext.debug.resolvedrequest.device.ua").String()
-				ip := value.Path("ext.debug.resolvedrequest.device.ip").String()
-				host_to_bidresp_tracker := value.Path("page_name").String()
-				pingdom_in_bidresp := 0
-				if host_to_bidresp_tracker == "\"pingdom.the-ozone-project.com\"" {
-					c, _ := statsd.New(statsd_host)
-					// Prefix every metric with the app name
-					pingdom_in_bidresp++
-					c.Namespace = "logger."
-					c.Incr("pingdom_to_bidresp_snplow", nil, float64(pingdom_in_bidresp))
-				}
-				user_id := value.Path("user.id").String()
-				contextArray := []sp.SelfDescribingJson{
-					*sp.InitSelfDescribingJson(
-						"iglu:tech.hereford/bidresponses_context/jsonschema/1-0-1",
-						map[string]interface{}{
-							"userid_ctxt": user_id,
-						},
-					),
-				}
-				subject.SetUseragent(ua)
-				subject.SetIpAddress(ip)
-				sdj := sp.InitSelfDescribingJson("iglu:tech.hereford/bidresponses/jsonschema/1-0-1", dataMap)
-				tracker.TrackSelfDescribingEvent(sp.SelfDescribingEvent{Event: sdj, Contexts: contextArray})
-				fmt.Println(data)
+				if resptopic == "bidresponse" {
+					data := string(p[3])
+					dataMap := make(map[string]interface{})
+					err := json.Unmarshal([]byte(data), &dataMap)
+					if err != nil {
+						fmt.Println(err)
+					}
+					respmsg := &sarama.ProducerMessage{
+						Topic: string(resptopic),
+						Value: sarama.StringEncoder(p[3]),
+					}
+					respproducer.Input() <- respmsg
+					value, _ := gabs.ParseJSON([]byte(data))
+					ua := value.Path("ext.debug.resolvedrequest.device.ua").String()
+					ip := value.Path("ext.debug.resolvedrequest.device.ip").String()
+					host_to_bidresp_tracker := value.Path("page_name").String()
+					pingdom_in_bidresp := 0
+					if host_to_bidresp_tracker == "\"pingdom.the-ozone-project.com\"" {
+						c, _ := statsd.New(statsd_host)
+						// Prefix every metric with the app name
+						pingdom_in_bidresp++
+						c.Namespace = "logger."
+						c.Incr("pingdom_to_bidresp_snplow", nil, float64(pingdom_in_bidresp))
+					}
+					user_id := value.Path("user.id").String()
+					contextArray := []sp.SelfDescribingJson{
+						*sp.InitSelfDescribingJson(
+							"iglu:tech.hereford/bidresponses_context/jsonschema/1-0-1",
+							map[string]interface{}{
+								"userid_ctxt": user_id,
+							},
+						),
+					}
+					subject.SetUseragent(ua)
+					subject.SetIpAddress(ip)
+					sdj := sp.InitSelfDescribingJson("iglu:tech.hereford/bidresponses/jsonschema/1-0-1", dataMap)
+					tracker.TrackSelfDescribingEvent(sp.SelfDescribingEvent{Event: sdj, Contexts: contextArray})
+					fmt.Println(data)
 				}
 			}
 			fmt.Println(count)
 			count++
 		}
-				if errs := reqproducer.Close(); errs != nil {
-					for _, err := range errs.(sarama.ProducerErrors) {
-						fmt.Println("Write to kafka failed: ", err)
-					}
-				}
-				if errs := respproducer.Close(); errs != nil {
-					for _, err := range errs.(sarama.ProducerErrors) {
-						fmt.Println("Write to kafka failed: ", err)
-					}
-				}
-}()
+		if errs := reqproducer.Close(); errs != nil {
+			for _, err := range errs.(sarama.ProducerErrors) {
+				fmt.Println("Write to kafka failed: ", err)
+			}
+		}
+		if errs := respproducer.Close(); errs != nil {
+			for _, err := range errs.(sarama.ProducerErrors) {
+				fmt.Println("Write to kafka failed: ", err)
+			}
+		}
+	}()
 
 	wg.Wait()
 }
